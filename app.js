6614fb11323c27a70fad159389cbea0596ab22bb
Andersson M <anderson.moreno.9@gmail.com>
2024-01-25 13:37:47 -0500

    Portfolio v1

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let name1 = 'mundo';
-document.write('Hola ' + name1);
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        // Ocultar el loader
+        document.querySelector(".loader").style.display = "none";
+        document.querySelector("#text-loader").style.display = "none";
+        document.querySelector("body").style.backgroundColor = "#9bfab0";
+        document.querySelector("body").style.margin = "14px";
+        document.querySelector("body").style.color = "#fff";
+        document.querySelector("#main-content").style.display = "block";
+    }, 1500);//3500 mls = 3.5s
+});
